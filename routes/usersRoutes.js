7a5d937ebea07150ea35a6f35b538902fa8a13bc
Bhavesh588 <34418394+Bhavesh588@users.noreply.github.com>
2021-06-15 08:12:07 +0530

    Fixing image save

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -60,7 +60,7 @@ async function sendMail(email, firstname, lastname, token) {
 
 const storage = multer.diskStorage({
 destination: function (req, file, cb) {
-	cb(null, "./uploads/");
+	cb(null, "../../client/public/uploads/");
 },
 filename: function (req, file, cb) {
 	cb(null, file.originalname);
